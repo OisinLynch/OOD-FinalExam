67fa5e5e555497547fe467d8de97b103c3810dd2
Oisin Lynch <oisinlynch1999@hotmail.com>
2020-05-13 11:32:40 +0100

    Completed question 5

--- OisinLynch_S00189006/Phone.cs
+++ OisinLynch_S00189006/Phone.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,16 +11,41 @@ namespace OisinLynch_S00189006
     public class Phone
     {
         //Properties
+        [Key]
         public string Name { get; set; }
         public decimal Price { get; set; }
         public string OperatingSystem { get; set; }
         public string OS_Image { get; set; }
         public string Phone_Image { get; set; }
 
+        //Cpntructors
+        public Phone(string name, decimal price, string operatingSystem, string os_Image, string phone_Image)
+        {
+            Name = name;
+            Price = price;
+            OperatingSystem = operatingSystem;
+            OS_Image = os_Image;
+            Phone_Image = phone_Image;
+        }
+
+        public Phone()
+        {
+
+        }
+
         //Methods 
         public void IncreasePrice(decimal percentValue)
         {
-            Price += percentValue / 100;
+            decimal value = Price * percentValue;
+            Price += value;
         }
     }
+
+    public class PhoneData : DbContext
+    {
+        public PhoneData():base("MyPhoneData") { }
+
+        public DbSet<Phone> Phones { get; set; }
+    }
+
 }
