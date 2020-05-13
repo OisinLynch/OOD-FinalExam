e52985dd7f596e81b50d083117192df526006fb0
Oisin Lynch <oisinlynch1999@hotmail.com>
2020-05-13 12:45:17 +0100

    Attempted question 7-9 but could not get them working

--- OisinLynch_S00189006/MainWindow.xaml.cs
+++ OisinLynch_S00189006/MainWindow.xaml.cs
@@ -22,6 +22,8 @@ namespace OisinLynch_S00189006
     {
         PhoneData db = new PhoneData();
 
+        List<Phone> phones = new List<Phone>();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -35,7 +37,17 @@ namespace OisinLynch_S00189006
             var phones = query.ToList();
 
             lbxPhones.ItemsSource = query.ToList();
-            
+
+            //DataTemplate
+
+            Phone p1 = new Phone("Samsung S20", 500, "Android", "/images/android.png", "/images/s20.jpg");
+            Phone p2 = new Phone("iPhone 11", 600, "IOS", "/images/apple.png", "/images/iphone11.png");
+
+            db.Phones.Add(p1);
+            db.Phones.Add(p2);
+
+            lbxPhones.ItemsSource = phones;
+
         }
 
         private void LbxPhones_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -47,6 +59,14 @@ namespace OisinLynch_S00189006
             string selected = lbxPhones.SelectedItem as string;
 
             tblkPrice.Text = selected;
+
+            var query2 = from p in db.Phones
+                          orderby p.Phone_Image
+                          select p.Phone_Image;
+
+            Image selectedImage = lbxPhones.SelectedItem as Image;
+
+            imgPhone = selectedImage;
         }
     }
 }
