289534af7e00e8d31d0f33d791d1f7360661a346
Oisin Lynch <oisinlynch1999@hotmail.com>
2020-05-13 10:20:23 +0100

    Completed question 2 and 3

---
+++ OisinLynch_S00189006/Phone.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OisinLynch_S00189006
+{
+    class Phone
+    {
+        //Properties
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string OperatingSystem { get; set; }
+        public string OS_Image { get; set; }
+        public string Phone_Image { get; set; }
+
+        //Methods 
+        public void IncreasePrice(decimal percentageValue)
+        {
+            Price += percentageValue;
+        }
+    }
+}
